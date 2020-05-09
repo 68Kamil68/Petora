fc0b95457f5e9c816bdd3f0830a47ee19d569977
Kamil <tret1lus@gmail.com>
2020-05-09 03:30:14 +0200

    add profile and follows models

--- server/code/petora/users/models.py
+++ server/code/petora/users/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=250)
+    created_at = models.DateField(default=timezone.now())
+    is_private = models.BooleanField(default=False)
+    is_enabled = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name + ' profile'
+
+
+class Follows(models.Model):
+    followed_profile = models.ForeignKey(Profile, on_delete=models.DO_NOTHING, related_name='followed')
+    followed_by = models.ForeignKey(Profile, on_delete=models.DO_NOTHING, related_name='followed_by')
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
